0db71028bb27258b3ee55e1daabdd273d2223c0a
Seminviso1 <oscarcarneiro100@gmail.com>
2025-11-20 11:23:25 -0300

    comeco da pagina

---
+++ site_simcc/src/pages/home/index.jsx
@@ -0,0 +1,25 @@
+import './style.css'
+
+function Home() {
+
+  return (
+
+    <>
+    <div className='Centro'>
+      <h1 className='titulo'>Vitrine SIMCC</h1>
+    </div>
+
+    <div className='botoes'>
+        <button type='button'>Patentes</button>
+        <button type='button'>Marcas</button>
+        <button type='button'>Desenhos industriais</button>
+        <button type='button'>Softwares</button>
+        <button type='button'>Topografia de Circuitos</button>
+        <button type='button'>Indicações Geográficas</button>
+    </div>
+
+    </>
+  )
+}
+
+export default Home
